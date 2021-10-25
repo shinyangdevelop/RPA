3de184fe7086f07bdeb2c9c29850a0621ad7deea
shinyangdevelop <shinyangdevelop@gmail.com>
2021-10-25 23:04:26 +0900

    Added code that the mouse automatically finds the 'Commit.png', and clicks it.

--- rpa_basic/2_desktop/6_image_recognition.py
+++ rpa_basic/2_desktop/6_image_recognition.py
@@ -1,5 +1,9 @@
 import pyautogui
 
-file_menu = pyautogui.locateOnScreen("file_menu.png")
-print(file_menu)
-pyautogui.click(file_menu)
+# file_menu = pyautogui.locateOnScreen("file_menu.png")
+# print(file_menu)
+# pyautogui.click(file_menu)
+
+file_commit = pyautogui.locateOnScreen("file_commit.png")
+print(file_commit)
+pyautogui.click(file_commit)
