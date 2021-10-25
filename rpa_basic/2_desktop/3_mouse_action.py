17c5f7b03e4bb0f17622031c7a7b6052c813a4d7
shinyangdevelop <shinyangdevelop@gmail.com>
2021-10-25 22:18:49 +0900

    Added doubleClick function and click function with a 'clicks' which clicks at current position for inputted times.

--- rpa_basic/2_desktop/3_mouse_action.py
+++ rpa_basic/2_desktop/3_mouse_action.py
@@ -1,9 +1,12 @@
 import pyautogui
 
-# pyautogui.sleep(3)
+pyautogui.sleep(3)
 # print(pyautogui.position())
 
-pyautogui.click(50, 10, duration=1)
-pyautogui.click()
-pyautogui.mouseDown()
-pyautogui.mouseUp()
+# pyautogui.click(50, 10, duration=1)
+# pyautogui.click()
+# pyautogui.mouseDown()
+# pyautogui.mouseUp()
+
+# pyautogui.doubleClick()
+# pyautogui.click(clicks=500)
