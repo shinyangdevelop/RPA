847e0311c3d70300bd3301311c51ea877b6bf7ee
shinyangdevelop <shinyangdevelop@gmail.com>
2021-10-25 21:55:57 +0900

    Added move function and codes that print cursor's location

--- rpa_basic/2_desktop/2_mouse_move.py
+++ rpa_basic/2_desktop/2_mouse_move.py
@@ -1,4 +1,16 @@
 import pyautogui
 
-pyautogui.moveTo(200, 200)
-pyautogui.moveTo()
+# pyautogui.moveTo(200, 200)
+# pyautogui.moveTo(100, 200, duration=5)
+
+# pyautogui.moveTo(100, 100, duration=0.25)
+# pyautogui.moveTo(200, 200, duration=0.25)
+# pyautogui.moveTo(300, 300, duration=0.25)
+
+# pyautogui.moveTo(100, 100, duration=0.25)
+# pyautogui.move(100, 100, duration=0.25)
+# pyautogui.move(100, 100, duration=0.25)
+
+p = pyautogui.position()
+print(p[0], p[1])
+print(p.x, p.y)
